d380adde4eb57877b5f61c6f4901ff74478b215d
Nortum <varevtsev@yandex.ru>
2022-04-19 20:22:24 +0400

    Initial commit

---
+++ src/main/java/web/dao/UserDaoImp.java
@@ -0,0 +1,44 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    protected EntityManager getEntityManager() {
+        return this.entityManager;
+    }
+
+    @Override
+    public void addUser(User user) {
+        getEntityManager().persist(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        getEntityManager().remove(getEntityManager().find(User.class, id));
+    }
+
+    @Override
+    public void editUser(User user) {
+        getEntityManager().merge(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return getEntityManager().find(User.class, id);
+    }
+
+    @Override
+    public List getAllUsers() {
+        return getEntityManager().createQuery("From User").getResultList();
+    }
+}
